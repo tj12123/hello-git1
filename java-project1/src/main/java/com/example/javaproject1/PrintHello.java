77fa2abfb7bf8672906397bd9aecc958a191e77d
tj12123 <1woadnjs@naver.com>
2023-04-18 16:35:40 +0900

    java 1

---
+++ java-project1/src/main/java/com/example/javaproject1/PrintHello.java
@@ -0,0 +1,8 @@
+package com.example.javaproject1;
+
+public class PrintHello {
+    public void print() {
+        System.out.println(1 + 1);
+
+    }
+}
---
+++ java-project1/src/main/java/com/example/javaproject1/PrintHelloTest.java
@@ -0,0 +1,8 @@
+package com.example.javaproject1;
+
+public class PrintHelloTest {
+    public static void main(String[] args) {
+        PrintHello printHello = new PrintHello();
+        printHello.print();
+    }
+}
